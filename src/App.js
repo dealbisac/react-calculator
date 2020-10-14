c152d3d731dd55b896657ca9fba349182cf5c8d6
dealbisac <chand.dipendra19@gmail.com>
2020-10-15 01:27:01 +0545

    input field

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './css/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+
+    this.state = {
+      current: '',
+      previous: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Calculator</h1>
+        <input className="result" type="text" value={this.state.current} />
       </div>
     )
   }
